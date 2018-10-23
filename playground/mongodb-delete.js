641e93c62ed171423f5d05a625fca6fa1946624d
Robert M <roo1@gmx.at>
2018-10-23 07:07:47 +0200

    Add update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,12 +10,22 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", {useNewUrlParser: true}
     const db =client.db('TodoApp');
 
     // deleteMany
-    db.collection('Todos').deleteMany({text: 'Nothing'}).then((result) => ){
-      console.log(result);
-    });
+    // db.collection('Todos').deleteMany({text: 'Nothing'}).then((result) => {
+    //     console.log(result);
+    // });
     // deleteOne
-
+    // db.collection('Todos').deleteOne({text: 'Something to do 4'}).then((result) => {
+    //     console.log(result);
+    // });
     // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Todos').findOneAndDelete({_id: new ObjectID("5bce9c6c59e43c1970f8477f")}).then((result) => {
+        console.log(result);
+    });
+
 
     //client.close();
 });
