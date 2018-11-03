cba55068fab1134f60640529332716a0d96b9170
Robert M <roo1@gmx.at>
2018-11-03 17:56:55 +0100

    Test POST /todos route with error

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var bosyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-app.use(bosyParser.json());
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
@@ -15,10 +15,8 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     },  (e) => {
-        res.sttus(400).send(e);
+        res.satus(400).send(e);
     });
-
-
 });
 
 
@@ -26,4 +24,4 @@ app.listen(3000, () => {
     console.log('Started on Port 3000');
 });
 
-
+module.exports ={app};
