b018490fa8dedcec0f67edd17b9683f23b5fd2a4
Robert M <roo1@gmx.at>
2018-11-04 07:21:29 +0100

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: "First test todo"
+}, {
+    text: "Second test todo"
+}];
+
+
 beforeEach((done) => {
-    Todo.deleteMany().then(() => done());
+    Todo.deleteMany().then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST / todos', () => {
@@ -24,7 +33,7 @@ describe('POST / todos', () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -36,17 +45,29 @@ describe('POST / todos', () => {
         request(app)
             .post('/todos') //url
             .send({})
-            .expect(400) // 400 comes back from server
+            .expect(200) // 400 comes back from server
             .end((err, res) => {
                 if(err){
                     return done(err); // return stops the script; done for properly failed test
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0); // database has no entry because we send bad data
+                    expect(todos.length).toBe(3); // database has no entry because we send bad data
                     done();
                 }).catch((e) => done(e));
             });
 
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+            expect(res.body.todos.length).toBe(2)
+            })
+            .end(done);
     });
 });
